b37c7a3aeee5003b66399e147ab47df8cb91e381
amine-tlight <amine.tlight1@gmail.com>
2016-10-22 19:37:31 +0100

    menu

--- src/test/main.java
+++ src/test/main.java
@@ -1,28 +1,75 @@
 package test;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Personne p1 = new Personne("mohammed","alg�rien");
-	
-		
-		Personne p2 = new Personne("omar","alg�rien");
-		
-		
-		Personne p3 = new Personne("tom","am�ricain");
-		
-		
-		
-		
+		Scanner r = new Scanner(System.in);
 		Amis a = new Amis();
+		int i;
+		
+		String input;
+		
 		a.creerListeAmis();
-		a.ajouterAmis(p1);
-		a.ajouterAmis(p2);
-		a.ajouterAmis(p3);
-		a.amisAlgerien();
-		a.amisEtranger();
+		do{
+			
+			System.out.println("----Main Menu----");
+			System.out.println("1)-Ajouter une Personne.");
+			System.out.println("2)-Voir Liste des Personns.");
+			System.out.println("3)-Voir Liste des Personnes Algériens.");
+			System.out.println("4)-Voir Liste des Personnes étrangers.");
+			System.out.println("5)-Quitter.");
+			i = r.nextInt();
+
+			if(i==1){
+				Personne p1 = new Personne("","");
+				do{
+				System.out.print("Entrer le nom: ");
+				input = r.nextLine();
+				}while(input.equals(""));
+				p1.nom=input;
+				
+				
+				do{
+					System.out.print("Entrer le nationalité: ");
+					input = r.nextLine();
+					p1.nationalité=input;
+					}while(input.equals(""));
+					
+				a.ajouterAmis(p1);
+				
+			}
+			
+			if(i==2){
+				System.out.println("----Liste D'amis----");
+				if(a.nbAmis==-1){
+					System.out.println("Liste vide...");
+				}else{
+					for (int j = 0; j <= a.nbAmis; j++) {
+					System.out.println("Amis "+ (j+1) + ": "+ a.amis[j].nom + " | nationalité: "+ a.amis[j].nationalité);
+					}
+				}
+			}
+			if(i==3){
+				a.amisAlgerien();
+				if(a.nbAmis==-1){
+					System.out.println("Liste vide...");
+				}
+			}
+			if(i==4){
+				a.amisEtranger();
+				if(a.nbAmis==-1){
+					System.out.println("Liste vide...");
+				}
+			}
+			if(i==1){
+				
+			}
+			
+		}while(i!=5);
+		
 	}
 
 }
